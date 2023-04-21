e210de2a681be18c37a66db7c7175c0d2aeec7cc
brennanp93 <brennan.p.perez@gmail.com>
2023-04-21 15:11:00 -0700

    additional styling

--- src/UpcomingEvents.jsx
+++ src/UpcomingEvents.jsx
@@ -1,18 +1,19 @@
 import { events } from "../data";
 export default function UpcomingEvents() {
-
   return (
     <>
-        <h1 id="events-title">Upcoming Events</h1>
+      <h1 id="events-title">Upcoming Events</h1>
       <div className="events-box">
         {events.map((e, idx) => (
           <div className="event-card" ket={idx}>
-            <h3>{e.name}</h3>
-            <h2>Date: {e.date}</h2>
-            <ul>
-              <li>{e.locationName}</li>
-              <li>{e.locationAddress}</li>
-            </ul>
+            {/* <h3>{e.name}</h3> */}
+            <h2>When: {e.date}</h2>
+
+            <h4>
+              Where: {e.locationName}&nbsp;
+              <br />
+              {e.locationAddress}
+            </h4>
             <p>{e.info}</p>
           </div>
         ))}
