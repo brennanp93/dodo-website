a7bf25fdb09cd9a115695a16e072ea2336a47ad0
brennanp93 <brennan.p.perez@gmail.com>
2023-04-21 22:50:42 -0700

    redo css to build mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-import "./App.css";
+// import "./App.css";
+import "./newApp.css";
 import Header from "./Header";
 import EmailForm from "./EmailForm";
 import AboutSection from "./AboutSection";
