0a2111f29113284f388c752e36b95215a8d2d2a5
brennanp93 <brennan.p.perez@gmail.com>
2023-04-21 18:37:50 -0700

    adding hyper link address for event

--- src/UpcomingEvents.jsx
+++ src/UpcomingEvents.jsx
@@ -12,7 +12,10 @@ export default function UpcomingEvents() {
             <h4>
               Where: {e.locationName}&nbsp;
               <br />
+              <a href="https://maps.apple.com/place?q=Provisions%20Deli%20%26%20Bottle%20Shop&ll=33.789014162248925%2C-117.85288989543915&auid=16839856032145497476&lsp=9902&address=143%20N%20Glassell%20St%2C%20Orange%2C%20CA%2092866%2C%20United%20States" target="_blank">
+
               {e.locationAddress}
+              </a>
             </h4>
             <p>{e.info}</p>
           </div>
